cd5b6bad588f45702780fbac680b0b63bb1302f2
Andre Benegas Pinho Amador <apinho-a@c1r5s11.42lisboa.com>
2026-05-10 18:35:20 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -6,71 +6,100 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 18:34:11 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/05/10 15:33:15 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/05/10 18:34:39 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		ft_word_counter(char const *s, char c)
+static int	ft_word_counter(char const *s, char c)
 {
 	int	word_counter;
-	
+	int	in_word;
+
 	word_counter = 0;
-	while (*s != 0 && c != 0)
+	in_word = 0;
+	while (*s != 0)
 	{
-		if (*s == c && *(s + 1) != c && *(s + 1) != 0)
+		if (*s == c)
+			in_word = 0;
+		if (*s != c && in_word == 0)
+		{
 			word_counter++;
+			in_word = 1;
+		}
 		s++;
 	}
 	return (word_counter);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**ft_calloc_error_handler(char **ptr_strings, int index_ptr)
 {
-char	**ptr_strings;
-char	*string;
-int		word_counter;
-int		index_ptr;
-int		i;
-int		j;
-int		k;
-
-if (!s || !c)
-	return((char **) ft_calloc(1, sizeof(char*)));
-word_counter = ft_word_counter(s, c);
-ptr_strings = (char **) ft_calloc(word_counter + 1, sizeof(char*));
-if (!ptr_strings)
-	return (NULL);
-index_ptr = 0;
-i = 0;
-while (s[i] != 0 && c != 0)
-{
-	while (s[i] == c && s[i + 1] == c)
-		i++;
-	if (s[i] == c && s[i + 1] != c)
+	while (index_ptr >= 0)
 	{
+		free(*(ptr_strings + index_ptr));
+		index_ptr--;
+	}
+	free(ptr_strings);
+	return (NULL);
+}
+
+static int	ft_string_filler(char **string, char const *s, int j, int i)
+{
+	int	k;
+
+	k = 0;
+	while (k < j)
+	{
+		*(*string + k) = s[i];
+		k++;
 		i++;
+	}
+	return (i);
+}
+
+char	**ft_split_actual(char **ptr_strings, char const *s,
+						char c, int index_ptr)
+{
+	char	*string;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] == 0)
+			break ;
 		j = 0;
 		while (s[i + j] != c && s[i + j] != 0)
 			j++;
-		if (s[i] == 0)
-			return (ptr_strings);
 		string = (char *) ft_calloc(j + 1, sizeof(char));
+		if (!string)
+			return (ft_calloc_error_handler(ptr_strings, index_ptr));
 		*(ptr_strings + index_ptr) = string;
 		index_ptr++;
-		if (!string)
-			return (ptr_strings);
-		k = 0;
-		while (k < j)
-		{
-			string[k] = s[i];
-			k++;
-			i++;
-		}
+		i = ft_string_filler(&string, s, j, i);
+		j = 0;
 	}
+	return (ptr_strings);
 }
-return (ptr_strings);
+
+char	**ft_split(char const *s, char c)
+{
+	char	**ptr_strings;
+	int		word_counter;
+	int		index_ptr;
+
+	if (!s)
+		return (NULL);
+	word_counter = ft_word_counter(s, c);
+	ptr_strings = (char **) ft_calloc(word_counter + 1, sizeof(char *));
+	index_ptr = 0;
+	if (!ptr_strings)
+		return (ft_calloc_error_handler(ptr_strings, index_ptr));
+	return (ft_split_actual(ptr_strings, s, c, index_ptr));
 }
 
 /* int	main ()
@@ -78,6 +107,11 @@ return (ptr_strings);
 	char **ptr_strings;
 
 	ptr_strings = ft_split(NULL, 0);
+	if (!ptr_strings)
+	{
+		printf("%s\n", "NULl was passed");
+		return (0);		
+	}
 	while (*ptr_strings != 0)
 	{
 		printf("%s\n", *ptr_strings);
